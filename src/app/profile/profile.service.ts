4413784c383a26f408c785d26833e4daff400a54
nguetcheu <dnguetcheu@gmail.com>
2021-12-04 23:52:03 +0100

    portfolio angular bootstrap

---
+++ src/app/profile/profile.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+    providedIn: 'root'
+  })
+  export class ProfileService {
+  // @ts-ignore
+    baseUrl = environment.baseUrl;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  contactus(data: any): Observable<any> {
+    return this.http.post('http://localhost:4200/contact',data);
+  }
+  }
